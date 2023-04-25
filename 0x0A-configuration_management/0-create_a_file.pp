30091f5772c432b5e7bfe133ca245c3bcde7b442
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-04-25 12:17:22 +0100

    Created new file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creating a file in /tmp Using Puppet
+
+file { '0-create_a_file':
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
