bf9c9dc4b82ee37ebea5d06c8127c4c2628ef192
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-12 15:59:51 +0100

    Update puppet script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,45 +1,13 @@
-# This script installs Nginx using puppet and sets up a custom 301 redirect
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html;
 
-# Install Nginx
-package {'nginx':
-  ensure => 'present',
-}
+    server_name _;
 
-# Set up custom index.html file
-file {'/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-}
-
-# Set up custom Nginx configuration file
-file {'/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => template('nginx/custom-config.erb'),
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  require => Package['nginx'],
-}
-
-# Enable custom site and disable default site
-file {'/etc/nginx/sites-enabled/default':
-  ensure  => 'absent',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-file {'/etc/nginx/sites-enabled/custom':
-  ensure  => 'link',
-  target  => '/etc/nginx/sites-available/custom',
-  require => File['/etc/nginx/sites-available/custom'],
-}
-
-# Restart Nginx service
-service {'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => [File['/etc/nginx/sites-available/custom'], File['/var/www/html/index.html']],
+    location /redirect_me {
+        return 301 https://fusedtc.tech/;
+    }
 }
 
