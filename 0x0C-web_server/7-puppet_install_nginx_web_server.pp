6065967cdb8ae6e4d5941b53a6857a3a1e2f8a2e
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-12 15:47:47 +0100

    custom puppet script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,45 @@
-class nginx {
-package { 'nginx':
-ensure => installed,
+# This script installs Nginx using puppet and sets up a custom 301 redirect
+
+# Install Nginx
+package {'nginx':
+  ensure => 'present',
 }
 
-service { 'nginx':
-ensure => running,
-require => Package['nginx'],
-}
-}
-
-class nginx::site {
-file_line { 'redirect':
-ensure => 'present',
-path => '/etc/nginx/sites-available/default',
-after => 'listen 80 default_server;',
-line => 'rewrite ^/redirect_me https://fusedtc.tech permanent;',
+# Set up custom index.html file
+file {'/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
 }
 
-file { '/var/www/html/index.html':
-content => 'Hello World!',
-}
+# Set up custom Nginx configuration file
+file {'/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => template('nginx/custom-config.erb'),
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  require => Package['nginx'],
 }
 
-include nginx
-include nginx::site
+# Enable custom site and disable default site
+file {'/etc/nginx/sites-enabled/default':
+  ensure  => 'absent',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+file {'/etc/nginx/sites-enabled/custom':
+  ensure  => 'link',
+  target  => '/etc/nginx/sites-available/custom',
+  require => File['/etc/nginx/sites-available/custom'],
+}
+
+# Restart Nginx service
+service {'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => [File['/etc/nginx/sites-available/custom'], File['/var/www/html/index.html']],
+}
+
